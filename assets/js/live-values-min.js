f0d18e475d25e5cfb9c8974141b95e6b8a5a0854
manujuniris <manuj@uniris.io>
2021-12-09 20:16:09 +0530

    tokenomics-section

--- assets/js/live-values-min.js
+++ assets/js/live-values-min.js
@@ -1,5 +1,6 @@
 var current_price = 0,
- current_circulating_supply = 0;
+ current_circulating_supply = 0,
+ market_cap=0;
 
 async function getstats() {
     const e = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=uniris&vs_currencies=usd"),
@@ -8,11 +9,14 @@ async function getstats() {
     const c = await fetch("https://ck7163lr1l.execute-api.us-east-1.amazonaws.com/default/uniris_circulating_supply"),
         r = await c.json();
     current_circulating_supply = r.circulating_supply;
+    market_cap = current_price*current_circulating_supply;
+    
 }
 async function displaystats() {
     await getstats(); 
     document.querySelector("#live-feed-price").innerText = current_price;
     document.querySelector("#current-circulating-supply").innerText = calc_M_or_B(current_circulating_supply);
+    document.querySelector("#market-cap").innerText = calc_M_or_B(market_cap);
 }
 
 function calc_M_or_B(e) {
