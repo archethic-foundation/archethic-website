65f3d3cedb65a05e748680f52877c1fbe902b603
manujuniris <manuj@uniris.io>
2021-12-13 10:43:13 +0530

    fixed-tokenomics

--- assets/js/live-values-min.js
+++ assets/js/live-values-min.js
@@ -1,4 +1,4 @@
-var current_price = 0,
+/*var current_price = 0,
  current_circulating_supply = 0,
  market_cap=0;
 
@@ -25,6 +25,36 @@ function calc_M_or_B(e) {
 
 let promise =displaystats();
 promise.then(
-    function(value) { /* code if successful */ },
-    function(error) { /* code if some error */ }
+    function(value) {  },
+    function(error) { }
+  );*/
+  var current_price = 0,
+ current_circulating_supply = 0,
+ market_cap=0;
+
+async function getstats() {
+    const e = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=uniris&vs_currencies=usd"),
+        t = await e.json();
+    current_price = t.uniris.usd;
+    const c = await fetch("https://ck7163lr1l.execute-api.us-east-1.amazonaws.com/default/uniris_circulating_supply"),
+        r = await c.json();
+    current_circulating_supply = 216000000;
+    market_cap = current_price*current_circulating_supply;
+    
+}
+async function displaystats() {
+    await getstats(); 
+    document.querySelector("#live-feed-price").innerText = current_price;
+    document.querySelector("#current-circulating-supply").innerText = calc_M_or_B(current_circulating_supply);
+    document.querySelector("#market-cap").innerText = calc_M_or_B(market_cap);
+}
+
+function calc_M_or_B(e) {
+    return isNaN(e) ? e : e < 9999 ? e : e < 1e6 ? Math.round(e / 1e3) + "K" : e < 1e7 ? (e / 1e6).toFixed(2) + "M" : e < 1e9 ? Math.round(e / 1e6) + "M" : e < 1e12 ? Math.round(e / 1e9) + "B" : "1T+"
+}
+
+let promise =displaystats();
+promise.then(
+    function(value) {  },
+    function(error) { }
   );
