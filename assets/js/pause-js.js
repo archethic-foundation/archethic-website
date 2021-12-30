8192356a62f741f59a4569894bc4779193c5f770
manujuniris <manuj@uniris.io>
2021-12-30 14:33:10 +0530

    fixed-video-bug

--- assets/js/pause-js.js
+++ assets/js/pause-js.js
@@ -2,19 +2,19 @@ var stopButton = document.getElementById('video-close-button-id');
 
 stopButton.onclick = function() {
   var myPlayer = document.getElementById("youtube-player-id"); 
-  myPlayer.setAttribute("src", " ");
+  myPlayer.setAttribute("src", "https://www.youtube.com/embed/AGM3osVKn24?autoplay=0&enablejsapi=1");
 };
 
 var stopButton1 = document.getElementById('video-close-button-id1');
 
 stopButton1.onclick = function() {
   var myPlayer1 = document.getElementById("youtube-player-id1"); 
-  myPlayer1.setAttribute("src", " ");
+  myPlayer1.setAttribute("src", "https://www.youtube.com/embed/WH-0bZeeHsY?autoplay=0&enablejsapi=1");
 };
 
 var stopButton2 = document.getElementById('video-close-button-id2');
 
 stopButton2.onclick = function() {
   var myPlayer2 = document.getElementById("youtube-player-id2"); 
-  myPlayer2.setAttribute("src", " ");
+  myPlayer2.setAttribute("src", "https://www.youtube.com/embed/K1JZVosWgQo?autoplay=0&enablejsapi=1");
 };
