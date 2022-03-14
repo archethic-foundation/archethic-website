e241dacb0bb195099127c1bde65a701e9f5cb95f
manujuniris <manuj@uniris.io>
2022-03-14 17:34:42 +0530

    added-aeweb & brand page

---
+++ assets/js/download.js
@@ -0,0 +1,16 @@
+function downloadImage(url, name){
+    fetch(url)
+      .then(resp => resp.blob())
+      .then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          // the filename you want
+          a.download = name;
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+      })
+      .catch(() => alert('An error sorry'));
+}
