a5e99c5928aa8a54c16bbf471ed7cad02ce25bf7
redDwarf03 <sylvain@archethic.net>
2024-02-10 10:23:30 +0100

    🐛 Fix page's structure

--- src/app/home/DApps/DApps.tsx
+++ src/app/home/DApps/DApps.tsx
@@ -5,6 +5,7 @@ import { ExternalLinks, InternalLinks } from '@/config'
 import CtaCardApp from '@/ui/CtaCardApp/CtaCardApp'
 import styles from './DApps.module.scss'
 import { T } from '@/ui/Text/Text'
+import { Parallax } from '@/ui/Parallax/Parallax'
 import { MaxWidthLayoutContainer } from '@/ui/MaxWidthLayoutContainer/MaxWidthLayoutContainer'
 interface DAppsProps {
   className?: string
@@ -13,60 +14,62 @@ interface DAppsProps {
 export default function DApps({ className }: DAppsProps) {
 
   return (
-    <>
-      <MaxWidthLayoutContainer className={styles.content}>
+    <section id='DApps' className={styles.section}>
+      <Parallax speed={4}>
 
-        <T as='h2' size='headline-regular'>
-          Dive deep into the next-gen blockchain – Get started with Archethic DApps.
-        </T>
-        <br />
-        <div className={styles.cards}>
-          <CtaCardApp
-            title='aeBridge'
-            description='Discover a seamless transfer of assets'
-            button={{ link: ExternalLinks.Bridge, label: '' }}
-            variantColor='raspberry'
-            env='MAINNET'
-          />
-          <CtaCardApp
-            title='aeWallet'
-            description='The first fully decentralized wallet'
-            button={{ link: InternalLinks.Wallet, label: '' }}
-            variantColor='raspberry'
-            env='MAINNET'
-          />
-          <CtaCardApp
-            title='aeExplorer'
-            description='Your gateway to transparency and discovery'
-            button={{ link: ExternalLinks.aeExpplorer, label: '' }}
-            variantColor='raspberry'
-            env='MAINNET'
-          />
-          <CtaCardApp
-            title='aeSwap'
-            description='Swap assets on-chain, add liquidity & access yield farming'
-            button={{ link: ExternalLinks.aeSwapTestnet, label: '' }}
-            variantColor='black'
-            env='TESTNET'
-          />
-          <CtaCardApp
-            title='aeHosting'
-            description='Free your content, forever with decentralized web hosting'
-            button={{ link: ExternalLinks.aeHostingTestnet, label: '' }}
-            variantColor='black'
-            env='TESTNET'
-          />
-          <CtaCardApp
-            title='aePlayground'
-            description='Turn your ideas into smart contracts – no expertise required'
-            button={{ link: ExternalLinks.aePlaygroundTestnet, label: '' }}
-            variantColor='black'
-            env='TESTNET'
-          />
+        <MaxWidthLayoutContainer className={styles.content}>
 
-        </div>
-      </MaxWidthLayoutContainer >
+          <T as='h2' size='headline-regular'>
+            Dive deep into the next-gen blockchain – Get started with Archethic DApps.
+          </T>
+          <br />
+          <div className={styles.cards}>
+            <CtaCardApp
+              title='aeBridge'
+              description='Discover a seamless transfer of assets'
+              button={{ link: ExternalLinks.Bridge, label: '' }}
+              variantColor='raspberry'
+              env='MAINNET'
+            />
+            <CtaCardApp
+              title='aeWallet'
+              description='The first fully decentralized wallet'
+              button={{ link: InternalLinks.Wallet, label: '' }}
+              variantColor='raspberry'
+              env='MAINNET'
+            />
+            <CtaCardApp
+              title='aeExplorer'
+              description='Your gateway to transparency and discovery'
+              button={{ link: ExternalLinks.aeExpplorer, label: '' }}
+              variantColor='raspberry'
+              env='MAINNET'
+            />
+            <CtaCardApp
+              title='aeSwap'
+              description='Swap assets on-chain, add liquidity & access yield farming'
+              button={{ link: ExternalLinks.aeSwapTestnet, label: '' }}
+              variantColor='black'
+              env='TESTNET'
+            />
+            <CtaCardApp
+              title='aeHosting'
+              description='Free your content, forever with decentralized web hosting'
+              button={{ link: ExternalLinks.aeHostingTestnet, label: '' }}
+              variantColor='black'
+              env='TESTNET'
+            />
+            <CtaCardApp
+              title='aePlayground'
+              description='Turn your ideas into smart contracts – no expertise required'
+              button={{ link: ExternalLinks.aePlaygroundTestnet, label: '' }}
+              variantColor='black'
+              env='TESTNET'
+            />
 
-    </>
+          </div>
+        </MaxWidthLayoutContainer >
+      </Parallax>
+    </section>
   )
 }
