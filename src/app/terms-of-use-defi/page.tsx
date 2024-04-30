ab1bb3b0ff97af60022c3c6579aeff87dda82ff4
redDwarf03 <sylvain@archethic.net>
2024-04-30 13:54:01 +0200

    📝 Add legal pages for DeFi

---
+++ src/app/terms-of-use-defi/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import Flex from '@/ui/Flex/Flex'
+import { MaxWidthLayoutContainer } from '@/ui/MaxWidthLayoutContainer/MaxWidthLayoutContainer'
+import { T } from '@/ui/Text/Text'
+
+import styles from './page.module.scss'
+
+export default function TermsOfUseDeFi() {
+  return (
+    <div className={styles.container}>
+      <MaxWidthLayoutContainer>
+        <Flex gap={32} smGap={16}>
+          <T as='h2' size='display-large' weight='semibold'>
+            Terms of use for the Archethic DeFi
+          </T>
+
+          <T as='h2' size='headline-medium'>
+            Last Updated: TbD
+          </T>
+
+          <div>
+            <T as='div' size='headline-medium-small'>
+              In progress
+            </T>
+          </div>
+        </Flex>
+      </MaxWidthLayoutContainer>
+    </div >
+  )
+}
